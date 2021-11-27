dcaf150d9afde2e56115bc7428f4a304183f5ec7
ZarusILie <lazarusliekarsono@gmail.com>
2021-11-27 10:51:49 +0700

    CS modul9 Lazaruslie Karsono

---
+++ app/src/main/java/LazaruslieJmartKD/jmart_android/model/Account.java
@@ -0,0 +1,11 @@
+package LazaruslieJmartKD.jmart_android.model;
+
+import java.io.Serializable;
+
+public class Account implements Serializable {
+    public double balance;
+    public String email;
+    public String name;
+    public String password;
+    public Store store;
+}
